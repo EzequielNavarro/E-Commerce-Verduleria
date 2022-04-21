4e5929a64fcf09c5c6b5e2da29448e0cc3736271
Ezequiel Navarro <ezequiel_hurlingham@live.com.ar>
2022-04-21 18:41:15 -0300

    pequeños errores

--- src/main/java/Grupo3/Verduleria/Repositorios/RepositorioKilo.java
+++ src/main/java/Grupo3/Verduleria/Repositorios/RepositorioKilo.java
@@ -1,6 +1,5 @@
 package Grupo3.Verduleria.Repositorios;
 
-import Grupo3.Verduleria.Entidades.Clientes;
 import Grupo3.Verduleria.Entidades.ProductoKilo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
