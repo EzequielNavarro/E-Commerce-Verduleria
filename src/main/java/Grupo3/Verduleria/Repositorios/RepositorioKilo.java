1f0b166019807680b56ee68b9b7d26f0131bfe0f
Ezequiel Navarro <ezequiel_hurlingham@live.com.ar>
2022-04-21 18:34:19 -0300

    add querys buscarPorNombre y BuscarPorDNI

--- src/main/java/Grupo3/Verduleria/Repositorios/RepositorioKilo.java
+++ src/main/java/Grupo3/Verduleria/Repositorios/RepositorioKilo.java
@@ -1,10 +1,15 @@
 package Grupo3.Verduleria.Repositorios;
 
+import Grupo3.Verduleria.Entidades.Clientes;
 import Grupo3.Verduleria.Entidades.ProductoKilo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositorioKilo  extends JpaRepository<ProductoKilo, String>{
-
+    
+    @Query("SELECT k FROM ProductoKilo k WHERE nombre = :nombre")
+    public ProductoKilo buscarPorNombre(@Param("nombre")String nombre);
 }
--- src/main/java/Grupo3/Verduleria/Repositorios/RepositorioUnidad.java
+++ src/main/java/Grupo3/Verduleria/Repositorios/RepositorioUnidad.java
@@ -2,10 +2,13 @@ package Grupo3.Verduleria.Repositorios;
 
 import Grupo3.Verduleria.Entidades.ProductoUnidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface RepositorioUnidad  extends JpaRepository<ProductoUnidad, String>{
+public interface RepositorioUnidad extends JpaRepository<ProductoUnidad, String> {
 
+    @Query("SELECT u FROM ProductoUnidad u WHERE nombre = :nombre")
+    public ProductoUnidad buscarPorNombre(@Param("nombre") String nombre);
 }
