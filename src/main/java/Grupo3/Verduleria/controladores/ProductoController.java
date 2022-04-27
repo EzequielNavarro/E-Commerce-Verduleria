6f08fda10179584408049329c8e3e1294364e523
Ezequiel Navarro <ezequiel_hurlingham@live.com.ar>
2022-04-26 21:34:56 -0300

    Metodos add Producto y delete

--- src/main/java/Grupo3/Verduleria/controladores/ProductoController.java
+++ src/main/java/Grupo3/Verduleria/controladores/ProductoController.java
@@ -1,16 +1,51 @@
 package Grupo3.Verduleria.controladores;
 
+import Grupo3.Verduleria.Entidades.ProductoKilo;
+import Grupo3.Verduleria.Servicios.ServicioKilo;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/productoslista")
 public class ProductoController {
 
+    @Autowired
+    private ServicioKilo servicioKilo;
+    
+    
     @GetMapping("/productoslista")
-    public String listaProductos() {
+    public String listaProductos(ModelMap model) throws Exception {
+        List<ProductoKilo> listaKilo = servicioKilo.findAll();
+        model.addAttribute("lista", listaKilo);
         return "Lista_Productos.html";
     }
+
+    @PostMapping("/addproducto")
+    public String agregar(ModelMap model, @RequestParam String nombre, @RequestParam Integer precio, @RequestParam Integer kilo) {
+        try {
+            servicioKilo.save(nombre, precio, kilo);
+        } catch (Exception ex) {
+            model.addAttribute("error", ex.getMessage());
+            return "Lista_Productos.html";
+        }
+
+        return "Lista_Productos.html";
+    }
+
+    @PostMapping("/delproducto")
+    public String borrar(ModelMap model, @RequestParam String id) {
+        try {
+            servicioKilo.delete(id);
+        } catch (Exception ex) {
+            model.addAttribute("error", "Hubo un problema: " + ex.getMessage());
+        }
+        return "Lista_Productos.html";
+    }
+
 }
