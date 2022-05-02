5fe82d1df97ab98a15b9162bef6f6e778d64b1d8
Ezequiel Navarro <ezequiel_hurlingham@live.com.ar>
2022-05-02 20:11:16 -0300

    asd

--- src/main/java/Grupo3/Verduleria/VerduleriaApplication.java
+++ src/main/java/Grupo3/Verduleria/VerduleriaApplication.java
@@ -8,14 +8,7 @@ public class VerduleriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerduleriaApplication.class, args);
-                // SOY EL DEVLOP
-                //Hola soy Ezequiel
-                // rapido
-                //hola soy gaston
-                //holasoysofi
-                //intento rodri
-                //Pruba 22/4/2022 00:19
-                //error
+                //asd
 	}
 
 }
