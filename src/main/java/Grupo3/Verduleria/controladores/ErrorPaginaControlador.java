446990bb50375a50e62bcd433c4de9eda3613eb3
Ezequiel Navarro <ezequiel_hurlingham@live.com.ar>
2022-05-06 06:33:40 -0300

    arregle htmls perfil y rutas , edit ErrorPaginaController

--- src/main/java/Grupo3/Verduleria/controladores/ErrorPaginaControlador.java
+++ src/main/java/Grupo3/Verduleria/controladores/ErrorPaginaControlador.java
@@ -1,54 +1,56 @@
-package Grupo3.Verduleria.controladores;
-
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-public class ErrorPaginaControlador implements ErrorController {
-
-    @RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
-        ModelAndView errorPage = new ModelAndView("error");
-        String errorMsg = "";
-        int httpErrorCode = getErrorCode(httpRequest);
-
-        switch (httpErrorCode) {
-            case 400:
-                errorMsg = "El recurso solicitado no existe.";
-                break;
-            case 401:
-                errorMsg = "No se encuentra autorizado.";
-                break;
-            case 403:
-                errorMsg = "No tiene permisos para acceder al sitio.";
-                break;
-            case 404:
-                errorMsg = "El recurso solicitado no fue encontrado.";
-                break;
-            case 405:
-                errorMsg = "El metodo no se encuentra permitido.";
-                break;
-            case 500:
-                errorMsg = "Ocurrió un error interno.";
-                break;
-
-        }
-        errorPage.addObject("codigo", "Error " + httpErrorCode);
-        errorPage.addObject("mensaje", errorMsg);
-        return errorPage;
-    }
-
-    private int getErrorCode(HttpServletRequest httpRequest) {
-        Map mapa = httpRequest.getParameterMap();
-        for (Object key : mapa.keySet()) {
-            String[] valores = (String[]) mapa.get(key);
-            for (String valor : valores) {
-                System.out.println(key.toString() + ": " + valor);
-            }
-        }
-        return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
-    }
-}
+//package Grupo3.Verduleria.controladores;
+//
+//import java.util.Map;
+//import javax.servlet.http.HttpServletRequest;
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class ErrorPaginaControlador implements ErrorController {
+//
+//    @RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST})
+//    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+//        ModelAndView errorPage = new ModelAndView("error");
+//        String errorMsg = "";
+//        int httpErrorCode = getErrorCode(httpRequest);
+//
+//        switch (httpErrorCode) {
+//            case 400:
+//                errorMsg = "El recurso solicitado no existe.";
+//                break;
+//            case 401:
+//                errorMsg = "No se encuentra autorizado.";
+//                break;
+//            case 403:
+//                errorMsg = "No tiene permisos para acceder al sitio.";
+//                break;
+//            case 404:
+//                errorMsg = "El recurso solicitado no fue encontrado.";
+//                break;
+//            case 405:
+//                errorMsg = "El metodo no está permitido.";
+//                break;
+//            case 500:
+//                errorMsg = "Ocurrió un error interno.";
+//                break;
+//
+//        }
+//        errorPage.addObject("codigo", "Error "+httpErrorCode);
+//        errorPage.addObject("mensaje", errorMsg);
+//        return errorPage;
+//    }
+//
+//    private int getErrorCode(HttpServletRequest httpRequest) {
+//        Map mapa = httpRequest.getParameterMap();
+//        for (Object key : mapa.keySet()) {
+//            String[] valores = (String[]) mapa.get(key);
+//            for (String valor : valores) {
+//                System.out.println(key.toString() + ": " + valor);
+//            }
+//        }
+//        return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+//    }
+//}
